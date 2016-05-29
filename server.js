90c4b1ec1f24440aa1fb706f938ee7ae52d87ddd
borgoff <borgoffbk@gmail.com>
2016-05-29 16:02:05 +0300

    tracing connections

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ io.on('connection', function(socket){
             });
             console.log(socket.id+'closed');
         });
+        console.log(socket.id+'connect');
 
         socket.on('disconnect', function () {
             socket.emit('disconnected');
