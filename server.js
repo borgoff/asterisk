190be35241c63b55420b9db401328f34f42a4886
serg <borgoffbk@gmail.com>
2016-06-02 11:08:27 +0300

    search user in base

--- server.js
+++ server.js
@@ -58,17 +58,28 @@ io.on('connection', function(socket){
         function(err, results){
             if (results){
                 //socket.emit('message',results);
-                connection.query('SELECT users.id, users_pi.fio, bills.deposit, users.credit, groups.name, districts.name, streets.name, builds.number, users_pi.address_flat'+
+                /*connection.query('SELECT users.id, users_pi.fio, bills.deposit, users.credit, tarif_plans.name, groups.name, districts.name, streets.name, builds.number, users_pi.address_flat'+
                 ' FROM (users'+
                 ' left join users_pi on users.uid = users_pi.uid'+
                 ' left join bills on users.uid = bills.uid'+
-                //' left join dv_main on dv_main.uid = users.uid )'+
-                //' left join tarif_plans on dv_main.tp_id = tarif_plans.id'+
+                ' left join dv_main on dv_main.uid = users.uid )'+
+                ' left join tarif_plans on dv_main.tp_id = tarif_plans.id'+
                 ' left join groups on users.gid = groups.gid'+
                 ' left join builds on users_pi.location_id = builds.id'+
                 ' left join streets on builds.street_id = streets.id'+
                 ' left join districts on streets.district_id = districts.id'+
                 ' WHERE users.uid = '+results.uid,
+                    function(err2, results2){
+                        if (results2){
+                            socket.emit('message',results2);
+                        }
+                        if (err2){
+                            socket.emit('message',err2);
+                        }
+                    });*/
+                connection.query('SELECT *'+
+                    ' FROM users'+
+                    ' LIMIT 1',
                     function(err2, results2){
                         if (results2){
                             socket.emit('message',results2);
