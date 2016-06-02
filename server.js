7c1a5eb14a4947665bcc2641e9a4992356df76ca
serg <borgoffbk@gmail.com>
2016-06-02 11:17:59 +0300

    search user in base

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on('connection', function(socket){
 
     //testing connection--------------------
     connection.query('SELECT *'+
-        ' FROM bills'+
+        ' FROM users'+
         ' ORDER BY uid DESC LIMIT 1',
         function(err, results){
             if (results){
